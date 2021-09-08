d12f409955a1b47179bd32dff29966690dc6458a
YongKeeTan <yongkee.t@gmail.com>
2021-09-08 16:47:29 +0800

    implemented index,create, show, edit tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # see all tasks
+  get '/tasks', to: 'tasks#index'
+  # create new task
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  # see one task
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  post 'tasks', to: 'tasks#create', as: :create_task
+
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/tasks/:id', to: 'tasks#update', as: :update_task
+
+  delete 'tasks/:id', to: 'task#destroy', as: :destroy_task
 end
